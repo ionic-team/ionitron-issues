3749b21e20864566539a9ba603776ef12f79c181
Drew Rygh <drew.rygh@gmail.com>
2014-11-29 21:04:30 -0600

    add ready label to defaults

--- config.py
+++ config.py
@@ -4,22 +4,33 @@ CONFIG_VARS = {
 
     'DEBUG': True,
 
-    'REPO_USERNAME': 'driftyco',               # user/organization name
-    'REPO_ID': 'ionic',                        # name of repo to watch
+    # user/organization name
+    'REPO_USERNAME': 'driftyco',
+    # name of repo to watch
+    'REPO_ID': 'ionic',
 
-    'GITHUB_USERNAME': EV('GITHUB_USERNAME'),  # bot's github username
-    'GITHUB_PASSWORD': EV('GITHUB_PASSWORD'),  # bot's github password
+    # bot's github username
+    'GITHUB_USERNAME': EV('GITHUB_USERNAME'),
+    # bot's github password
+    'GITHUB_PASSWORD': EV('GITHUB_PASSWORD'),
 
-    'WARN_INACTIVE_AFTER': 80,                 # warn after $X inactive days
-    'CLOSE_INACTIVE_AFTER': 87,                # close after $X inactive days
+    # warn after $X inactive days
+    'WARN_INACTIVE_AFTER': 80,
+    # close after $X inactive days
+    'CLOSE_INACTIVE_AFTER': 87,
 
-    'MAX_COMMENTS': 10,                        # ignore issues with $X+ comments
+    # ignore issues with $X+ comments
+    'MAX_COMMENTS': 10,
 
-    'LABEL_WHITELIST': ['*'],                  # only take action on issues with these labels
-    'LABEL_BLACKLIST': ['in progress'],        # ignore issues with these labels
+    # only take action on issues with these labels
+    'LABEL_WHITELIST': ['*'],
+    # ignore issues with these labels
+    'LABEL_BLACKLIST': ['in progress', 'ready'],
 
-    'ON_WARN_LABEL': 'ionitron:warned',        # label to add when issue is warned
-    'ON_CLOSE_LABEL': 'ionitron:closed',       # label to add when issue is closed
+    # label to add when issue is warned
+    'ON_WARN_LABEL': 'ionitron:warned',
+    # label to add when issue is closed
+    'ON_CLOSE_LABEL': 'ionitron:closed',
 
     'WARNING_TEMPLATE': 'https://raw.githubusercontent.com/driftyco/ionitron-lingo/master/templates/cron/warning.md',
     'CLOSING_TEMPLATE': 'https://raw.githubusercontent.com/driftyco/ionitron-lingo/master/templates/cron/closed.md',
