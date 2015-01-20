56cd719df6a8413d85c667c8fa782e2873980ce3
Adam Bradley <adambradley25@gmail.com>
2015-01-19 22:24:28 -0600

    hack

--- main.py
+++ main.py
@@ -26,6 +26,28 @@ db = SQLAlchemy(app)
 app.wsgi_app = heroku_bouncer.bouncer(app.wsgi_app)
 
 
+class IssueScore(db.Model):
+    organization = db.Column(db.String(80), primary_key=True)
+    repo = db.Column(db.String(80), primary_key=True)
+    issue_number = db.Column(db.Integer, primary_key=True)
+    score = db.Column(db.Integer)
+    title = db.Column(db.String(512))
+    comments = db.Column(db.Integer)
+    username = db.Column(db.String(512))
+    created = db.Column(db.String(512))
+    updated = db.Column(db.String(512))
+    avatar = db.Column(db.String(512))
+    score_data = db.Column(db.Text)
+    assignee = db.Column(db.String(512))
+
+    def __init__(self, organization, repo, issue_number):
+        self.organization = organization
+        self.repo = repo
+        self.issue_number = issue_number
+
+    def __repr__(self):
+        return '<IssueScore %r>' % self.number
+
 @app.route("/")
 def index():
     """
