1ee5f383e0a214a9bcf006843370e34d8f14af73
Adam Bradley <adambradley25@gmail.com>
2015-02-08 21:34:43 -0600

    derp

--- tasks/issue_scores.py
+++ tasks/issue_scores.py
@@ -76,7 +76,7 @@ def get_issue_scores():
         }
 
         open_issues = github_api.fetch_open_issues()
-        if not open_issues or not isinstance(open_issues, list)::
+        if not open_issues or not isinstance(open_issues, list):
             return { 'error': 'Unable to fetch open issues.' }
 
         for issue in open_issues:
